8a9a68626245c0fc1cc56406f394e56c767e5eb4
Stephanie Richards <skerichards@gmail.com>
2014-05-14 16:01:46 -0700

    updates to support more complex tables
    - changed call from text to html to support links
    - added option for tfoot cells to show only at the end of the output
    - modified list id options for more css flexibility

--- jquery.restable.js
+++ jquery.restable.js
@@ -1,4 +1,14 @@
 /*! ReStable v0.1.1 by Alessandro Benoit */
+
+/* --------
+    EDITED by skerichards 2014-01-17
+    changed cell calls from .text() to .html()
+    added option for a footer with a single cell which spans the full table
+    changed list ID output to reflect original element ID or class, or generic if neither is available
+
+    TO DO: thead needs support for multiple rows
+-------- */
+
 (function ($, window, i) {
 
     'use strict';
@@ -8,53 +18,91 @@
         // Settings
         var s = $.extend({
             rowHeaders: true,
+            singleFooter: false,
             maxWidth: 480
         }, options);
 
         // Build the responsive menu container and fill it with build_menu()
-        function create_responsive_table(element, i) {
+        function create_responsive_table(element) {
 
             var $cols = [],
                 $result = {},
-                $cols_header = $(element).find('tr').first().children('td,th'),
+                $cols_header = $(element).find('thead').find('tr').children('td,th'),
+                $cols_footer = $(element).find('tfoot').find('tr').children('td,th').html(),
                 $row_number = 0,
                 $list;
 
+            // header
             if (s.rowHeaders) {
                 $cols_header = $cols_header.slice(1);
             }
 
-            // Prende le intestazioni
             $cols_header.each(function () {
-                $cols.push($(this).text());
+                $cols.push($(this).html());
             });
+            // footer
+            if (s.singleFooter) {
 
-            // Costruisce un array con il contenuto: $result
-            $(element).find('tr').slice(1).each(function () {
+                // find only the tbody content
+                $(element).find('tbody').find('tr').slice(0).each(function () {
 
-                var $row = $(this);
-                $row_number += 1;
+                    var $row = $(this);
+                    $row_number += 1;
 
-                $.each($cols, function (index, value) {
+                    $.each($cols, function (index, value) {
+                        index += 1;
 
-                    index += 1;
+                        if (s.rowHeaders) {
+                            if (!$result[value]) { $result[value] = {}; }
+                            $result[value][$row.children('td:nth-child(1)').html()] = $row.children('td:nth-child(' + (index + 1) + ')').html();
+                        } else {
+                            if (!$result[$row_number]) { $result[$row_number] = {}; }
+                            $result[$row_number][value] = $row.children('td:nth-child(' + index + ')').html();
+                        }
 
-                    if (s.rowHeaders) {
-                        if (!$result[value]) { $result[value] = {}; }
-                        $result[value][$row.children('td:nth-child(1)').text()] = $row.children('td:nth-child(' + (index + 1) + ')').text();
-                    } else {
-                        if (!$result[$row_number]) { $result[$row_number] = {}; }
-                        $result[$row_number][value] = $row.children('td:nth-child(' + index + ')').text();
-                    }
+                    });
 
                 });
 
-            });
+            } else {
+
+                // Costruisce un array con il contenuto: $result
+                $(element).find('tr').slice(1).each(function () {
+
+                    var $row = $(this);
+                    $row_number += 1;
+
+                    $.each($cols, function (index, value) {
+
+                        index += 1;
+
+                        if (s.rowHeaders) {
+                            if (!$result[value]) { $result[value] = {}; }
+                            $result[value][$row.children('td:nth-child(1)').html()] = $row.children('td:nth-child(' + (index + 1) + ')').html();
+                        } else {
+                            if (!$result[$row_number]) { $result[$row_number] = {}; }
+                            $result[$row_number][value] = $row.children('td:nth-child(' + index + ')').html();
+                        }
+
+                    });
+
+                });
+
+            }
+
+            // get a name for the list
+            var $list_id = $(element).attr('id') + '-list';
+            if ($list_id === 'undefined-list') {
+                var $list_id = $(element).attr('class') + '-list';
+            }
+            if ($list_id === '-list') {
+                var $list_id = 'tabletolist';
+            }
 
             // Crea la lista
             $list = $('<ul/>', {
-                class: 'tabletolist ' + ((s.rowHeaders) ? 'rh' : 'nrh'),
-                id: 'tabletolist' + i
+                class: 'tabletolist ' + ((s.rowHeaders) ? 'rh' : 'nrh') + ' ' + ((s.singleFooter) ? 'sf' : 'nsf'),
+                id: $list_id
             }).insertBefore($(element));
             $.each($result, function (index, value) {
 
@@ -72,6 +120,12 @@
                 });
 
             });
+
+            if (s.singleFooter) {
+                $('<li/>', {
+                    html: $cols_footer
+                }).addClass('footnotes').appendTo($list);
+            }
 
             return $list;
 
@@ -126,4 +180,4 @@
         $('table').ReStable(options);
     };
 
-})(jQuery, this, 0);
+}(jQuery, this, 0));
